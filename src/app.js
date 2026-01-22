0dbe568305311d7b843bc4031bba6946551691db
Usuario LaBebita <labebita@example.com>
2026-01-21 20:40:12 -0500

    change: modifico mensaje principal

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hola mundo con Node y Git');
+  res.send('Hola mundo CAMBIO MALO');
 });
 
 app.get('/saludo', (req, res) => {
