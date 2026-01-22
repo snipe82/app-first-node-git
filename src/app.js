5dc37ba6c9c8cd51ceae801a3bc499de8f031f36
Usuario LaBebita <labebita@example.com>
2026-01-21 20:47:07 -0500

    Revert "feat: endpoint /saludo"
    This reverts commit 9ec6c6533aa5b5bdc1348d37390a9a623d3451b2.

--- src/app.js
+++ src/app.js
@@ -5,10 +5,6 @@ app.get('/', (req, res) => {
   res.send('Hola mundo CAMBIO MALO');
 });
 
-app.get('/saludo', (req, res) => {
-    res.send('Hola desde una nueva feature');
-  });
-  
 app.listen(3000, () => {
   console.log('Servidor corriendo en puerto 3000');
 });
