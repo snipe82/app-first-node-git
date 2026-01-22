1c8a9a4b4ebcd314f830a4590f9d356c647eb06c
Usuario LaBebita <labebita@example.com>
2026-01-21 21:32:45 -0500

    change: saludo local y probando conflicto

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hola saludo LOCAL');
+  res.send('Hola saludo LOCAL PROBANDO CONFLICTOS');
 });
 
 app.get('/saludo', (req, res) => {
