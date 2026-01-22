385772c0a1fd7af062b84d6a7ceba07aecabc667
Usuario LaBebita <labebita@example.com>
2026-01-21 21:12:11 -0500

    change: saludo version 2

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hola mundo CAMBIO MALO OTRA VEZ MEEEEEEEEE');
+  res.send('Hola saludo VERSION 2');
 });
 
 app.get('/saludo', (req, res) => {
